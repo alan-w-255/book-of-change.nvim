bd240272301e0047e7c38083dee4876018a579af
alan-w-255 <1649022341@qq.com>
2022-01-31 18:37:38 +0800

    feat: remove 随机选卦功能

--- lua/book-of-change/init.lua
+++ lua/book-of-change/init.lua
@@ -1,27 +1,7 @@
-local M={}
-local zhouyi_text = require('book-of-change.zhouyi_text')
-local ZHOUYI_LINE_COUNT = #zhouyi_text
 local gua = require'book-of-change.gua'
 
+local M={}
 local bagua = {'乾', '兑', '离', '震','巽','坎','艮', '坤'}
-
-local function random_seed()
-    local dev_rand = io.open("/dev/urandom", 'r')
-    if dev_rand ~= nil then
-        local d=dev_rand:read(4)
-        math.randomseed(os.time()+d:byte(1)+(d:byte(2)*256)+(d:byte(3)*65536)+(d:byte(4)*4294967296))
-    else
-        math.randomseed(tostring(os.time()):reverse():sub(1, 7))
-    end
-end
-
-
-function M.suan_ming()
-    random_seed()
-    local random_line = math.random(ZHOUYI_LINE_COUNT)
-    local result = zhouyi_text[random_line]
-    return result
-end
 
 function M.suan_ming_by_date(year, month, day, hour)
     local upGua = (year + month + day)%8
